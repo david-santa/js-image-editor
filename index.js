18804dc8a03c7e9dfd7f3d1370ad11b9e27791d5
David <santadavid18@stud.ase.ro>
2020-11-16 01:01:54 +0200

    Selection working for static canvas

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var xStart = 0;
+var yStart = 0;
+var xEnd = 0;
+var yEnd = 0;
+
 document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
   const dropZoneElement = inputElement.closest(".drop-zone");
 
@@ -34,20 +39,13 @@ document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
     e.preventDefault();
     if (e.dataTransfer.files.length) {
       inputElement = inputElement.files = e.dataTransfer.files;
-      var can = document.getElementById("canvas");
+      var canvas = document.getElementById("canvas");
       var ctx = document.getElementById("canvas").getContext("2d");
       var img = new Image();
       img.onload = function () {
-        ctx.drawImage(img, 0, 0, can.width, can.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        const data = imageData.data;
-        for (var i = 0; i < data.length; i += 4) {
-          var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
-          data[i] = avg; // red
-          data[i + 1] = avg; // green
-          data[i + 2] = avg; // blue
-          ctx.putImageData(imageData, 0, 0);
-        }
+        //albNegru(imageData);
       };
       img.src = URL.createObjectURL(e.dataTransfer.files[0]);
       changeMode();
@@ -89,3 +87,58 @@ function changeMode() {
   let formElement = document.querySelector(".form-drop");
   formElement.remove();
 }
+
+function albNegru(imageData) {
+  var ctx = document.getElementById("canvas").getContext("2d");
+  const data = imageData.data;
+  for (var i = 0; i < data.length; i += 4) {
+    var avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    data[i] = avg; // red
+    data[i + 1] = avg; // green
+    data[i + 2] = avg; // blue
+    ctx.putImageData(imageData, 0, 0);
+  }
+}
+
+const canvas = document.querySelector(".canvas");
+canvas.addEventListener("mousedown", (e) => {
+  var cRect = canvas.getBoundingClientRect(); // Gets CSS pos, and width/height
+  var canvasX = Math.round(e.clientX - canvas.offsetLeft); // Subtract the 'left' of the canvas
+  var canvasY = Math.round(e.clientY - canvas.offsetTop);
+  xStart = canvasX;
+  yStart = canvasY;
+  console.log(xStart, yStart);
+  // var context = canvas.getContext("2d");
+  // context.beginPath();
+  // context.moveTo(x, y);
+  // context.lineTo(0, 0);
+  // context.stroke();
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  var cRect = canvas.getBoundingClientRect(); // Gets CSS pos, and width/height
+  var canvasX = Math.round(e.clientX - canvas.offsetLeft); // Subtract the 'left' of the canvas
+  var canvasY = Math.round(e.clientY - canvas.offsetTop);
+  xEnd = canvasX;
+  yEnd = canvasY;
+});
+
+document.addEventListener("keydown", logKey);
+
+function logKey(e) {
+  if (e.code === `KeyA`) {
+    canvas
+      .getContext("2d")
+      .fillRect(xStart, yStart, xEnd - xStart, yEnd - yStart);
+    console.log(xEnd, yEnd);
+    console.log(xStart, yStart, xEnd - xStart, yEnd - yStart);
+  }
+}
+
+canvas.addEventListener("mousemove", function (e) {
+  var cRect = canvas.getBoundingClientRect(); // Gets CSS pos, and width/height
+  var canvasX = Math.round(e.clientX - cRect.left); // Subtract the 'left' of the canvas
+  var canvasY = Math.round(e.clientY - cRect.top); // from the X/Y positions to make
+  canvas.getContext("2d").clearRect(0, 0, 100, 30); // (0,0) the top left of the canvas
+  canvas.getContext("2d").fillText("X: " + canvasX + ", Y: " + canvasY, 10, 20);
+});
